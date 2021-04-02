05590767e2a664d52584425526eccc736923ddab
Teodil <47178606+Teodil@users.noreply.github.com>
2021-04-03 02:00:10 +1000

    Добавил Стрейфы

---
+++ GameJam/Assets/Scripts/Extensions/GetSign.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MathExten
+{
+    public static float GetSign(float value)
+    {
+        if (value > 0)
+            return 1;
+        if (value < 0)
+            return -1;
+        else
+            return 0;
+    }
+    public static int GetSign(int value)
+    {
+        if (value > 0)
+            return 1;
+        if (value < 0)
+            return -1;
+        else
+            return 0;
+    }
+    public static double GetSign(double value)
+    {
+        if (value > 0)
+            return 1;
+        if (value < 0)
+            return -1;
+        else
+            return 0;
+    }
+}
--- GameJam/Assets/Scripts/Player/Movement.cs
+++ GameJam/Assets/Scripts/Player/Movement.cs
@@ -10,14 +10,23 @@ public class Movement : MonoBehaviour
     private float right;
 
     [SerializeField]
-    private float speed = 5f;
-    [SerializeField]
-    private float JumpForce = 20f;
+    private bool isClicked = false;
 
+    [Header("Настройка скорости")]
+    [SerializeField]
+    private float speed = 5f;
+
+    [Header("Настройки Стрейфа")]
     [SerializeField]
     private float timerFoDoubleClick = 0;
     [SerializeField]
-    private float maxTimeForDoubleClick = 0.2f;
+    private float maxTimeForDoubleClick = 0.01f;
+    [SerializeField]
+    private float strafeForce = 10f;
+    [SerializeField]
+    private float reloadStrafeTime = 5f;
+    [SerializeField]
+    private float currentReloadStrafeTime = 0;
 
     void Start()
     {
@@ -27,26 +36,56 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))//Обработка Нажатия на клавиши
+        {
+            if (!isClicked)//Проверка была ли нажата до этого клавиша
+            {
+                Debug.Log("Обновление таймера");
+                timerFoDoubleClick = 0;
+                isClicked = true;
+            }
+            else
+            {
+                if (timerFoDoubleClick < maxTimeForDoubleClick && currentReloadStrafeTime <= 0)
+                    Strafe();
+                else
+                    isClicked = false;
+            }
+
+        }
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
         {
+            if (currentReloadStrafeTime < reloadStrafeTime-0.2f)
+            {
                 right = speed * Input.GetAxis("Horizontal");
                 rigidbody.velocity = new Vector2(right, rigidbody.velocity.y);
                 timerFoDoubleClick += Time.deltaTime;
+            }
         }
-        if((Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D)) && timerFoDoubleClick < maxTimeForDoubleClick)
-        {
-            timerFoDoubleClick = 0;
-            rigidbody.AddForce(new Vector2(5 * Input.GetAxis("Horizontal"), 0), ForceMode2D.Impulse);
-        }
-
-
     }
 
+    private void Strafe()
+    {
+        rigidbody.velocity = new Vector2(0, rigidbody.velocity.y);
+        float accelerate = strafeForce * MathExten.GetSign(Input.GetAxis("Horizontal"));
+        Debug.Log("Ускорение " + accelerate);
+        rigidbody.AddForce(new Vector2(accelerate, 0), ForceMode2D.Impulse);
+        currentReloadStrafeTime = reloadStrafeTime;
+        StartCoroutine(ReloadStrafeCoroutine());
+    }
+
+    private IEnumerator ReloadStrafeCoroutine()
+    {
+        while(currentReloadStrafeTime > 0)
+        {
+            currentReloadStrafeTime -= Time.deltaTime;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
 
 
     public void Jump(float jumpForce)
     {
-        Debug.Log("прышок сила " + jumpForce);
         rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpForce);
         Debug.Log(rigidbody.velocity);
     }
