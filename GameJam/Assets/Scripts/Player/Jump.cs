6bcad2b4b8d9459ec15bb4d2a451445b532ceff9
Teodil <47178606+Teodil@users.noreply.github.com>
2021-04-02 22:38:16 +1000

    Added LeftRight Move

--- GameJam/Assets/Scripts/Player/Jump.cs
+++ GameJam/Assets/Scripts/Player/Jump.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class Hello : MonoBehaviour
+
+public class Jump : MonoBehaviour
 {
     // Start is called before the first frame update
+
+
     void Start()
     {
         
---
+++ GameJam/Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Rigidbody2D rigidbody;
+    private Jump jump;
+    private Vector2 right;
+
+    [SerializeField]
+    private float speed = 5f;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        right = transform.right * speed * Input.GetAxis("Horizontal");
+
+        rigidbody.velocity = right;
+        
+
+    }
+
+
+    public void Jump(Vector2 jumpForce)
+    {
+        rigidbody.velocity += jumpForce;
+    }
+
+}
