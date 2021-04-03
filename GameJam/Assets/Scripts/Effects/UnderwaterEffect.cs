c45d7599a9bc9f17da1432e2e12d4253df03b11a
aQCy <rox4388@gmail.com>
2021-04-03 19:09:03 +1000

    Background + Scene Update

--- GameJam/Assets/Scripts/Effects/UnderwaterEffect.cs
+++ GameJam/Assets/Scripts/Effects/UnderwaterEffect.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
- [ExecuteInEditMode, ImageEffectAllowedInSceneView]
 public class UnderwaterEffect : MonoBehaviour
 {
     public Material _mat;
