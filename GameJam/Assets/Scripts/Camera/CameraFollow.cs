766c098de61047833882b2c26f0d398bd04ba1a3
Teodil <47178606+Teodil@users.noreply.github.com>
2021-04-03 19:59:52 +1000

    Добавлена катсцена в начале

---
+++ GameJam/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Transform player;
+
+    [SerializeField]
+    private float xOffset = 22.72481f;
+    [SerializeField]
+    private float yOffset = 10.9f;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = player.position + new Vector3(xOffset, yOffset, transform.position.z);
+    }
+}
---
+++ GameJam/Assets/Scripts/Extensions/TimeLineFix.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+
+public class TimeLineFix : MonoBehaviour
+{
+    bool fix = false;
+    [SerializeField]
+    private Animator playerAnim;
+    [SerializeField]
+    private RuntimeAnimatorController playerContr;
+    [SerializeField]
+    private PlayableDirector director;
+
+    private void OnEnable()
+    {
+        playerContr = playerAnim.runtimeAnimatorController;
+        playerAnim.runtimeAnimatorController = null;
+    }
+
+    private void Update()
+    {
+        if(director.state != PlayState.Playing && !fix)
+        {
+            fix = true;
+            playerAnim.runtimeAnimatorController = playerContr;
+        }
+    }
+}
