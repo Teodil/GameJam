7e3e2f2911e7e5bd7adef65d024645621015821b
Teodil <47178606+Teodil@users.noreply.github.com>
2021-04-05 00:37:06 +1000

    AddedCutScene

---
+++ GameJam/Assets/Scripts/CutSceneStart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutSceneStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetTrigger("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
